73103593721587222cbbfcd38c57451aebbdefb5
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-02-17 14:41:37 +0530

    Initial Commit

---
+++ SIMRemovalDectectionDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SIMRemovalDectectionDemo
+//
+//  Created by Sagar Kadam on 17/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SIMRemovalDectectionDemo/SIMRemovalDectectionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIMRemovalDectectionDemoApp.swift
+//  SIMRemovalDectectionDemo
+//
+//  Created by Sagar Kadam on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SIMRemovalDectectionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
